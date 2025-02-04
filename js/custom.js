b8afe55349ba8c449081c0284e23ff2f43919435
Hammad Ali <hammad19ali@gmail.com>
2025-02-04 07:04:07 +0500

    model loaded using model-viewer

---
+++ js/custom.js
@@ -0,0 +1,18 @@
+AOS.init({ once: true })
+$(document).ready(function () {
+  var url = window.location.pathname;
+  var filename = url.substring(url.lastIndexOf('/') + 1);
+  if (filename == "") {
+    filename = "index.html"
+  }
+  $("header .nav-item .nav-link").removeClass("active");
+  $(`header .nav-item .nav-link[href="${filename}"]`).addClass("active")
+});
+$(window).scroll(function (e) {
+  var scroll = $(window).scrollTop();
+  if (scroll >= 100) {
+    $("header").addClass("scroll");
+  } else {
+    $("header").removeClass("scroll");
+  }
+});
